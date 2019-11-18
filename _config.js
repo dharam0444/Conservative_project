f8ac1a055a5b7e7b2d9fc8a58b9f06d072ffbc4e
dharam0444 <57313948+dharam0444@users.noreply.github.com>
2019-11-18 15:19:39 +0530

    Create _config.js

---
+++ _config.js
@@ -0,0 +1,19 @@
+var ids = {
+    github: {
+      clientID: 'get_your_own',
+      clientSecret: 'get_your_own',
+      callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+    },
+    linkedin: {
+      clientID: 'get_your_own',
+      clientSecret: 'get_your_own',
+      callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback"
+    },
+    twitter: {
+      consumerKey: 'LpiFlmHYOdamj53k3UzDDpHsl',
+      consumerSecret: 'tGmhZoWFd2ChUUojAMJlAfN26mIjxiS7oWYJ7wIHIkwZVZcieM',
+      callbackURL: "https://5aecc9b8.ngrok.io/sign"
+    }
+  };
+  
+  module.exports = ids;
